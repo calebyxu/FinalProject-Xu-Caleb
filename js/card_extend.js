57b612f12fc64891563671e940f501cf75baff50
Caleb Xu <calxu@iu.edu>
2024-12-06 02:16:33 -0500

    finished mobile site for home page

--- js/card_extend.js
+++ js/card_extend.js
@@ -1,13 +1,27 @@
 /* Event listeners */
+/* Card Extension */
 function extend(e) {
     e.currentTarget.classList.toggle('extend')
     e.currentTarget.classList.toggle('retract')
 }
 
+/* Mobile Nav */
+function mobileFunction(e) {
+    mobileNav.classList.toggle('mobile-extend')
+}
+
 /* Define vars */
+/* Card Extension */
 const cards = document.querySelectorAll('#description');
 
 for (var i = 0, len = cards.length; i < len; i++) {
     cards[i].addEventListener('click', extend)
 }
+
+/* Mobile Nav */
+const mobileNav = document.querySelector('nav')
+const mobileClose = document.querySelector('#x-icon')
+const mobileHamburger = document.querySelector("#mobile-hamburger")
+mobileClose.addEventListener('click', mobileFunction)
+mobileHamburger.addEventListener('click', mobileFunction)
 
