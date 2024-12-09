34445f6004ad63c14c67d7870e9805139a6aa3c5
Caleb Xu <calxu@iu.edu>
2024-12-09 14:15:39 -0500

    completed yesterday stuff

--- js/card_extend.js
+++ js/card_extend.js
@@ -5,6 +5,19 @@ function extend(e) {
     e.currentTarget.classList.toggle('retract')
 }
 
+function scroll(e) {
+    if (window.scrollY >= 700 && window.scrollY <= 1000 && flag == 0) {
+        cards[0].classList.toggle('extend')
+        cards[0].classList.toggle('retract')
+        cards[1].classList.toggle('extend')
+        cards[1].classList.toggle('retract')
+        cards[2].classList.toggle('extend')
+        cards[2].classList.toggle('retract')
+        flag = 1
+    }
+    
+}
+
 /* Mobile Nav */
 function mobileFunction(e) {
     mobileNav.classList.toggle('mobile-extend')
@@ -12,6 +25,7 @@ function mobileFunction(e) {
 
 /* Define vars */
 /* Card Extension */
+var flag = 0
 const cards = document.querySelectorAll('#description');
 
 for (var i = 0, len = cards.length; i < len; i++) {
@@ -25,3 +39,4 @@ const mobileHamburger = document.querySelector("#mobile-hamburger")
 mobileClose.addEventListener('click', mobileFunction)
 mobileHamburger.addEventListener('click', mobileFunction)
 
+const scrollTest = window.addEventListener('scroll', scroll)
